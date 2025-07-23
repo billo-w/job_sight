a595b0adf9f7aa9a31c93800da3425a891c94b49
billo <belalwaw@gmail.com>
2025-07-23 10:50:06 +0100

    setting debug to false on flask to prevent security issues

--- app.py
+++ app.py
@@ -281,4 +281,4 @@ def internal_error(error):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5000)
+    app.run(debug=False, port=5000)

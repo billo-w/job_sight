5ecd01de1f62cbcfe948ee434d5a42b909de2bae
billo-w <belalwaw@gmail.com>
2025-08-22 11:31:32 +0100

    Adding a testing deployment stage in CI CD

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,3 @@
 output "app_url" {
-  value = var.flask_env == "production" ? digitalocean_app.app[0].live_url : digitalocean_app.testing_app[0].live_url
+  value = length(digitalocean_app.app) > 0 ? digitalocean_app.app[0].live_url : digitalocean_app.testing_app[0].live_url
 }
